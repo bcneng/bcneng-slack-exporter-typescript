3190a6b997f88a9aff174e4c8fef7e5088066fa3
Alex <alexdelamo06@gmail.com>
2020-05-12 19:01:32 +0300

    Push

--- copy-data-to-dist.sh
+++ copy-data-to-dist.sh
@@ -1,5 +1,12 @@
 #!bin/bash
 
-echo "Hello, world!"
 cp -r data/ dist/
+for entry in dist/data/*
+do 
+    if [ -d "$entry" ]; then
+        for file in $entry/*; do
+            echo "${file##*/}"
+        done
+    fi
+done
 
