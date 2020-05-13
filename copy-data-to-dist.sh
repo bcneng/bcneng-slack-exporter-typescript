3505f04d31deded9a5d682834bf0567943035a52
Alex <alexdelamo06@gmail.com>
2020-05-13 19:23:29 +0300

    Added index generation of files per channel

--- copy-data-to-dist.sh
+++ copy-data-to-dist.sh
@@ -1,12 +1,22 @@
 #!bin/bash
-
+rm -rf dist/data
 cp -r data/ dist/
-for entry in dist/data/*
-do 
-    if [ -d "$entry" ]; then
-        for file in $entry/*; do
-            echo "${file##*/}"
-        done
-    fi
-done
 
+json_array() {
+  echo -n '['
+  while [ $# -gt 0 ]; do
+    x=${1//\\/\\\\} 
+    echo -n \"${x//\"/\\\"}\"
+    [ $# -gt 1 ] && echo -n ', '
+    shift
+  done
+  echo ']'
+}
+
+for dir in $(find dist/data/* -type d) 
+do
+    files=($dir/*)
+    files=(${files[@]##*/})
+    files=(${files[@]%.*})
+    json_array "${files[@]}" >> ${dir}/index.json
+done
